d66c4d0acfb1f9f4ab5e48102046831042141aa4
Coen <kruijf.coen@gmail.com>
2024-05-14 14:58:59 +0200

    changed secretkey config

--- src/util/config.js
+++ src/util/config.js
@@ -1,14 +1,10 @@
 //
 // Application configuration
 //
-const secretkey = process.env.SECRETKEY || 'DitIsEenGeheim'
+const secretkey = process.env.JWT_KEY || 'DitIsEenGeheim'
 
 const config = {
     secretkey: secretkey,
-
-    dbHost: 'localhost',
-    dbUser: 'app_user',
-    dbDatabase: 'database_name'
 }
 
 module.exports = config
